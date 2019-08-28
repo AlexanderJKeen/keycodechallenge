e0c6b5ce80714e663f5f8c120a72e267414b69d1
CN138 <cn138@CN0138.local>
2019-08-28 16:54:42 +0100

    firstUpload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const input3 = document.getElementById('input3') 
+
+document.addEventListener('keydown', () => {
+    input1.textContent = event.key
+    input2.textContent = event.code
+    input3.textContent = event.which
+})
